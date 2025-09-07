2d610733f4ed3340cd8d1792ad72bb4ce270a910
bell <bellelm@Mac.lan>
2025-09-07 19:27:56 +0100

    added task view for authenticated users only

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Task
+from .serialisers import TaskSerialiser
+from rest_framework import viewsets
+from rest_framework.permissions import(IsAuthenticated)
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset=Task.objects.all()
+    serializer_class=TaskSerialiser
+    permission_classes=[IsAuthenticated] 
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+    
